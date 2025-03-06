6f1054f77105efdace482744d169a5bd093bc222
YooJoonYoung <ypin1003@naver.com>
2025-03-06 21:25:01 +0900

    아이템 장착과 모션

--- Assets/Scripts/ScriptableObject/ItemData.cs
+++ Assets/Scripts/ScriptableObject/ItemData.cs
@@ -39,4 +39,7 @@ public class ItemData : ScriptableObject
 
     [Header("Consumable")]
     public ItemDataConsumbale[] consumables;
+
+    [Header("Equip")]
+    public GameObject equipPrefab;
 }
--- Assets/Scripts/UI/UIInventory.cs
+++ Assets/Scripts/UI/UIInventory.cs
@@ -27,6 +27,8 @@ public class UIInventory : MonoBehaviour
     ItemData selectedItem;
     int selectedItemIndex = 0;
 
+    int curEquipIndex;
+
     void Start()
     {
         controller = CharacterManager.Instance.Player.controller;
@@ -217,6 +219,38 @@ public class UIInventory : MonoBehaviour
         }
 
         UpdateUI();
+    }
+
+    public void OnEquipButton()
+    {
+        if (slots[curEquipIndex].equipped)
+        {
+           UnEquip(curEquipIndex);
+        }
+
+        slots[selectedItemIndex].equipped = true;
+        curEquipIndex = selectedItemIndex;
+        CharacterManager.Instance.Player.equip.EquipNew(selectedItem);
+        UpdateUI();
+
+        SelectItem(selectedItemIndex);
+    }
+
+    void UnEquip(int index)
+    {
+        slots[index].equipped = false;
+        CharacterManager.Instance.Player.equip.UnEquip();
+        UpdateUI();
+
+        if (selectedItemIndex == index)
+        {
+            SelectItem(selectedItemIndex);
+        }
+    }
+
+    public void OnUnEquipButton()
+    {
+       UnEquip(selectedItemIndex);    
     }
 }
 
