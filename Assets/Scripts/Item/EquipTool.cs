30297d6c8f68fa06ea3a8e840f3b59759d4c48fb
YooJoonYoung <ypin1003@naver.com>
2025-03-06 21:54:17 +0900

    자원 채취

--- Assets/Scripts/Item/EquipTool.cs
+++ Assets/Scripts/Item/EquipTool.cs
@@ -15,12 +15,14 @@ public class EquipTool : Equip
     public bool doesDealDamage;
     public int damage;
 
-    private Animator animator; 
+    private Animator animator;
+    private Camera camera;
 
-     void Start()           
-     {
+    void Start()
+    {
         animator = GetComponent<Animator>();
-     }
+        camera = Camera.main;
+    }
 
     public override void OnAttackInput()
     {
@@ -35,6 +37,20 @@ public class EquipTool : Equip
     void OnCanAttack()
     {
         attacking = false;
+    }
+
+    public void OnHit()
+    {
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, attackDistance))
+        {
+            if (doesGatherResources && hit.collider.TryGetComponent(out Resource resource))
+            {
+                resource.Gather(hit.point, hit.normal);
+            }
+        }
     }
 }
 
